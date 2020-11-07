c2cb8914a95fe8704939bd1c8385a9924c7cbcc2
Raghav Sharda <raghavsharda96@gmail.com>
2020-11-07 03:51:29 -0500

    extraction and tokenization done

---
+++ runner.py
@@ -0,0 +1,46 @@
+import sys, os, glob, fitz, textract
+from pathlib import Path
+from string import punctuation
+from nltk.tokenize import word_tokenize
+
+# For ref: https://usefulshortcuts.com/alt-codes/bullet-alt-codes.php
+def tokenize(pdfs_dict):
+    pdfs_dict_tokens = {}
+    remove_list = list(set(punctuation)) + ["♦", "•", "◘", "○", "◙", "--", "``", "–", "//", "’" , "●",]
+    for key, value in pdfs_dict.items():
+        list_tokens = word_tokenize(value)
+        list_tokens = list(filter(lambda i: i not in remove_list, list_tokens))
+        pdfs_dict_tokens[key] = list_tokens
+    return pdfs_dict_tokens
+
+# For ref: https://pymupdf.readthedocs.io/en/latest/document.html#document
+def readfiles():
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    file_data_path = os.path.join(fileDir, 'DATA')
+    file_keywords_path = os.path.join(fileDir, 'KEYWORDS')
+    pdf_text, pdfs_dict, keyword_list =  "", {}, []
+    for pdf_file in glob.glob(os.path.join(file_data_path, '*.pdf')):
+        with fitz.open(pdf_file) as doc:
+            # read each page
+            for page in doc: 
+                pdf_text += page.getText("text")
+        # get pdf file name
+        pdf_filename = os.path.basename(pdf_file)
+        # save the read pdf in dict
+        pdfs_dict[pdf_filename] = pdf_text
+    
+    for keyword_file in glob.glob(os.path.join(file_keywords_path, '*.txt')):
+        with open(keyword_file , 'r') as file:
+            keyword_list = file.read().split("\n")
+
+    return(pdfs_dict,keyword_list)
+
+def main():
+    pdfs_dict,keyword_list = readfiles()
+    pdfs_dict_tokens = tokenize(pdfs_dict)
+    print(pdfs_dict_tokens)
+    print("\n Keyword list")
+    print(keyword_list)
+
+if __name__ == "__main__":
+    main()
